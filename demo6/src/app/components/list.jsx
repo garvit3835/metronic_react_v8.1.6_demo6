b509249385fb7abfeee76c3092584d2e2ab2358f
garvit1189 <97427673+garvit3835@users.noreply.github.com>
2023-03-05 15:00:30 +0530

    final commit.

--- demo6/src/app/components/list.jsx
+++ demo6/src/app/components/list.jsx
@@ -11,8 +11,8 @@ export default function List({list, setList}) {
   const [email, setEmail] = useState('')
   const [salary, setSalary] = useState()
   const [country, setCountry] = useState('')
-  const [countryId, setCountryId] = useState()
-  const [stateId, setStateId] = useState()
+  const [countryId, setCountryId] = useState('')
+  const [stateId, setStateId] = useState('')
   const [state, setState] = useState('')
   const [city, setCity] = useState('')
   const countries = Country.getAllCountries()
@@ -69,7 +69,8 @@ export default function List({list, setList}) {
         theme: 'colored',
       })
     }
-
+    setState('')
+    setCity('')
     update.disabled = false
     update.innerText = 'Confirm Update'
   }
@@ -120,156 +121,168 @@ export default function List({list, setList}) {
           </thead>
           {/* <br /> */}
           {/* <tbody> */}
-            {list &&
-              list.map((employee, i) => {
-                return <tbody key={i}>
-                <tr>
-                      <td className='fs-3'>{i + 1}</td>
-                      <td className='fs-3'>{employee.name}</td>
-                      <td className='fs-3'>{employee.age}</td>
-                      <td className='fs-3'>{employee.email}</td>
-                      <td className='fs-3'>{employee.salary}</td>
-                      <td className='fs-3'>{employee.country}</td>
-                      <td className='fs-3'>{employee.state}</td>
-                      <td className='fs-3'>{employee.city}</td>
+          {list &&
+            list.map((employee, i) => {
+              return (
+                <tbody key={i}>
+                  <tr>
+                    <td className='fs-3'>{i + 1}</td>
+                    <td className='fs-3'>{employee.name}</td>
+                    <td className='fs-3'>{employee.age}</td>
+                    <td className='fs-3'>{employee.email}</td>
+                    <td className='fs-3'>{employee.salary}</td>
+                    <td className='fs-3'>{employee.country}</td>
+                    <td className='fs-3'>{employee.state}</td>
+                    <td className='fs-3'>{employee.city}</td>
 
-                      <td>
-                        <button
-                          type='button'
-                          className='btn btn-primary btn-sm py-1'
-                          onClick={() => {
-                            employee._id !== employeeId
-                              ? setEmployeeId(employee._id)
-                              : setEmployeeId()
-                          }}
-                        >
-                          Update
-                        </button>
+                    <td>
+                      <button
+                        type='button'
+                        className='btn btn-primary btn-sm py-1'
+                        onClick={async () => {
+                          if (employee._id !== employeeId) {
+                            setEmployeeId(employee._id)
+                            setName(employee.name)
+                            setAge(employee.age)
+                            setSalary(employee.salary)
+                            setEmail(employee.email)
+                          } else {
+                            setEmployeeId()
+                          }
+                        }}
+                      >
+                        Update
+                      </button>
+                    </td>
+
+                    <td>
+                      <button
+                        type='button'
+                        id={employee._id}
+                        className='btn btn-danger btn-sm py-1'
+                        onClick={() => {
+                          handleDelete(employee._id)
+                        }}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+
+                  {employeeId === employee._id ? (
+                    <tr>
+                      <th className='fs-3'></th>
+
+                      <td className='fs-4'>
+                        <input
+                          type='text'
+                          placeholder='Enter name'
+                          value={name}
+                          onChange={(e) => setName(e.target.value)}
+                          required
+                          className='form-control form-control-sm'
+                        />
                       </td>
-
-                      <td>
-                        <button
-                          type='button'
-                          id={employee._id}
-                          className='btn btn-danger btn-sm py-1'
-                          onClick={() => {
-                            handleDelete(employee._id)
-                          }}
+                      <td className='fs-4'>
+                        <input
+                          type='number'
+                          id='age'
+                          value={age}
+                          placeholder='Enter age'
+                          onChange={(e) => setAge(e.target.value)}
+                          min='10'
+                          required
+                          className='form-control form-control-sm w-100'
+                        />
+                      </td>
+                      <td className='fs-4'>
+                        <input
+                          type='email'
+                          placeholder='Enter email'
+                          value={email}
+                          onChange={(e) => setEmail(e.target.value)}
+                          required
+                          className='form-control form-control-sm w-100'
+                        />
+                      </td>
+                      <td className='fs-4'>
+                        <input
+                          type='number'
+                          placeholder='Enter salary'
+                          value={salary}
+                          onChange={(e) => setSalary(e.target.value)}
+                          required
+                          className='form-control form-control-sm w-100'
+                        />
+                      </td>
+                      <td className='fs-4'>
+                        <select
+                          onChange={handleCountry}
+                          defaultValue=''
+                          required
+                          className='form-control form-control-sm w-100'
                         >
-                          Delete
+                          <option value='' label='Select Country' disabled />
+                          {countries.map((country, i) => {
+                            return (
+                              <option
+                                key={i}
+                                value={`${country.name}%${country.isoCode}`}
+                                label={country.name}
+                              />
+                            )
+                          })}
+                        </select>
+                      </td>
+                      <td className='fs-4'>
+                        <select
+                          onChange={handleState}
+                          defaultValue=''
+                          className='form-control form-control-sm w-100'
+                        >
+                          <option value='' label='Select State' disabled />
+                          {states.map((state, i) => {
+                            return (
+                              <option
+                                key={i}
+                                value={`${state.name}%${state.isoCode}`}
+                                label={state.name}
+                              />
+                            )
+                          })}
+                        </select>
+                      </td>
+                      <td className='fs-4'>
+                        <select
+                          onChange={(e) => setCity(e.target.value)}
+                          defaultValue=''
+                          className='form-control form-control-sm w-100'
+                        >
+                          <option value='' label='Select City' disabled />
+                          {cities.map((city, i) => {
+                            return <option key={i} value={city.name} label={city.name} />
+                          })}
+                        </select>
+                      </td>
+                      <td colSpan='2'>
+                        <button
+                          id='update'
+                          type='submit'
+                          className='btn btn-primary btn-sm py-2'
+                          // onClick={handleUpdate}
+                          value='Confirm'
+                        >
+                          Confirm Update
                         </button>
                       </td>
                     </tr>
-
-                    {employeeId === employee._id ? (
-                      <tr>
-                        <th className='fs-3'></th>
-
-                        <td className='fs-4'>
-                          <input
-                            type='text'
-                            placeholder='Enter name'
-                            onChange={(e) => setName(e.target.value)}
-                            required
-                            className='form-control form-control-sm'
-                          />
-                        </td>
-                        <td className='fs-4'>
-                          <input
-                            type='number'
-                            id='age'
-                            placeholder='Enter age'
-                            onChange={(e) => setAge(e.target.value)}
-                            min='10'
-                            required
-                            className='form-control form-control-sm w-100'
-                          />
-                        </td>
-                        <td className='fs-4'>
-                          <input
-                            type='email'
-                            placeholder='Enter email'
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                            className='form-control form-control-sm w-100'
-                          />
-                        </td>
-                        <td className='fs-4'>
-                          <input
-                            type='number'
-                            placeholder='Enter salary'
-                            onChange={(e) => setSalary(e.target.value)}
-                            required
-                            className='form-control form-control-sm w-100'
-                          />
-                        </td>
-                        <td className='fs-4'>
-                          <select
-                            onChange={handleCountry}
-                            defaultValue=''
-                            required
-                            className='form-control form-control-sm w-100'
-                          >
-                            <option value='' label='Select Country' disabled />
-                            {countries.map((country, i) => {
-                              return (
-                                <option
-                                  key={i}
-                                  value={`${country.name}%${country.isoCode}`}
-                                  label={country.name}
-                                />
-                              )
-                            })}
-                          </select>
-                        </td>
-                        <td className='fs-4'>
-                          <select
-                            onChange={handleState}
-                            defaultValue=''
-                            className='form-control form-control-sm w-100'
-                          >
-                            <option value='' label='Select State' disabled />
-                            {states.map((state, i) => {
-                              return (
-                                <option
-                                  key={i}
-                                  value={`${state.name}%${state.isoCode}`}
-                                  label={state.name}
-                                />
-                              )
-                            })}
-                          </select>
-                        </td>
-                        <td className='fs-4'>
-                          <select
-                            onChange={(e) => setCity(e.target.value)}
-                            defaultValue=''
-                            className='form-control form-control-sm w-100'
-                          >
-                            <option value='' label='Select City' disabled />
-                            {cities.map((city, i) => {
-                              return <option key={i} value={city.name} label={city.name} />
-                            })}
-                          </select>
-                        </td>
-                        <td colSpan='2'>
-                          <button
-                            id='update'
-                            type='submit'
-                            className='btn btn-primary btn-sm py-2'
-                            // onClick={handleUpdate}
-                            value='Confirm'
-                          >
-                            Confirm Update
-                          </button>
-                        </td>
-                      </tr>
-                    ) : (
-                      ''
-                    )}
-                  </tbody>
-                // )
-              })}
+                  ) : (
+                    ''
+                  )}
+                </tbody>
+              )
+              // )
+            })}
           {/* </tbody> */}
         </table>
       </form>
